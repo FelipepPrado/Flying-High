63f7ecb68ca8bfbc8601c25bf20d6fe27e9c712f
FelipepPrado <felipepdelimaa@gmail.com>
2026-06-01 11:55:30 -0300

    Initial Commit

---
+++ FlyingHigh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlyingHigh
+//
+//  Created by Felipe Prado de Lima on 01/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlyingHigh/FlyingHighApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlyingHighApp.swift
+//  FlyingHigh
+//
+//  Created by Felipe Prado de Lima on 01/06/26.
+//
+
+import SwiftUI
+
+@main
+struct FlyingHighApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
