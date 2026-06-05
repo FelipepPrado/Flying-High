91faba240ce9d3c38a7de429606636dda4ad0b14
taiunara <anatainarabat@gmail.com>
2026-06-05 12:39:44 -0300

    feat(camera): implement complete camera feature with preview, photo capture, and save view

--- FlyingHigh/Model/CameraManager.swift
+++ FlyingHigh/Model/CameraManager.swift
@@ -1,18 +1,419 @@
 //
-//  CameraManager.swift
+//  CameraModel.swift
 //  FlyingHigh
 //
 //  Created by Tainara Nascimento on 05/06/26.
 //
 
-import SwiftUI
+import UIKit
+import AVFoundation
+internal import Combine
 
-struct CameraManager: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+class CameraManager: NSObject, ObservableObject {
+    
+    private let captureSession = AVCaptureSession()
+    
+    private var isCaptureSessionConfigured = false
+    private var deviceInput: AVCaptureDeviceInput?
+    private var photoOutput: AVCapturePhotoOutput?
+    @Published var flashMode: AVCaptureDevice.FlashMode = .off
+    
+    // for preview
+    private var videoOutput: AVCaptureVideoDataOutput?
+    private var sessionQueue: DispatchQueue!
+    
+    // device related
+    private var allCaptureDevices: [AVCaptureDevice] {
+        AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInTrueDepthCamera, .builtInDualCamera, .builtInDualWideCamera, .builtInWideAngleCamera, .builtInDualWideCamera], mediaType: .video, position: .unspecified).devices
+    }
+    
+    private var frontCaptureDevices: [AVCaptureDevice] {
+        allCaptureDevices
+            .filter { $0.position == .front }
+    }
+    
+    private var backCaptureDevices: [AVCaptureDevice] {
+        allCaptureDevices
+            .filter { $0.position == .back }
+    }
+    
+    private var captureDevices: [AVCaptureDevice] {
+        var devices = [AVCaptureDevice]()
+        if let backDevice = backCaptureDevices.first {
+            devices += [backDevice]
+        }
+        if let frontDevice = frontCaptureDevices.first {
+            devices += [frontDevice]
+        }
+        return devices
+    }
+    
+    private var availableCaptureDevices: [AVCaptureDevice] {
+        captureDevices
+            .filter( { $0.isConnected } )
+            .filter( { !$0.isSuspended } )
+    }
+    
+    private var captureDevice: AVCaptureDevice? {
+        didSet {
+            guard let captureDevice = captureDevice else { return }
+            sessionQueue.async {
+                self.updateSessionForCaptureDevice(captureDevice)
+            }
+        }
+    }
+        
+    var isRunning: Bool {
+        captureSession.isRunning
+    }
+    
+    var isUsingFrontCaptureDevice: Bool {
+        guard let captureDevice = captureDevice else { return false }
+        return frontCaptureDevices.contains(captureDevice)
+    }
+    
+    var isUsingBackCaptureDevice: Bool {
+        guard let captureDevice = captureDevice else { return false }
+        return backCaptureDevices.contains(captureDevice)
+    }
+    
+    // for capture photo
+    private var addToPhotoStream: ((AVCapturePhoto) -> Void)?
+    
+    lazy var photoStream: AsyncStream<AVCapturePhoto> = {
+        AsyncStream { continuation in
+            addToPhotoStream = { photo in
+                continuation.yield(photo)
+            }
+        }
+    }()
+    
+    // for preview device output
+    var isPreviewPaused = false
+
+    private var addToPreviewStream: ((CIImage) -> Void)?
+    
+    lazy var previewStream: AsyncStream<CIImage> = {
+        AsyncStream { continuation in
+            addToPreviewStream = { ciImage in
+                if !self.isPreviewPaused {
+                    continuation.yield(ciImage)
+                }
+            }
+        }
+    }()
+    
+    
+    override init() {
+        super.init()
+        // The value of this property is an AVCaptureSessionPreset indicating the current session preset in use by the receiver. The sessionPreset property may be set while the receiver is running.
+        captureSession.sessionPreset = .low
+        
+        sessionQueue = DispatchQueue(label: "session queue")
+        captureDevice = availableCaptureDevices.first ?? AVCaptureDevice.default(for: .video)
+        
+    }
+
+    func start() async {
+        let authorized = await checkAuthorization()
+        guard authorized else {
+            print("Camera access was not authorized.")
+            return
+        }
+        
+        if isCaptureSessionConfigured {
+            if !captureSession.isRunning {
+                sessionQueue.async { [self] in
+                    self.captureSession.startRunning()
+                }
+            }
+            return
+        }
+        
+        sessionQueue.async { [self] in
+            self.configureCaptureSession { success in
+                guard success else { return }
+                self.captureSession.startRunning()
+            }
+        }
+    }
+    
+    func stop() {
+        guard isCaptureSessionConfigured else { return }
+        
+        if captureSession.isRunning {
+            sessionQueue.async {
+                self.captureSession.stopRunning()
+            }
+        }
+    }
+    
+    // switch between available cameras
+    func switchCaptureDevice() {
+        if let captureDevice = captureDevice, let index = availableCaptureDevices.firstIndex(of: captureDevice) {
+            let nextIndex = (index + 1) % availableCaptureDevices.count
+            self.captureDevice = availableCaptureDevices[nextIndex]
+        } else {
+            self.captureDevice = AVCaptureDevice.default(for: .video)
+        }
+    }
+
+
+    
+    func takePhoto() {
+        guard let photoOutput = self.photoOutput else { return }
+        
+        sessionQueue.async {
+            var photoSettings = AVCapturePhotoSettings()
+
+            if photoOutput.availablePhotoCodecTypes.contains(.hevc) {
+                photoSettings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.hevc])
+            }
+            
+            let isFlashAvailable =
+                self.deviceInput?.device.isFlashAvailable ?? false
+
+            photoSettings.flashMode =
+            isFlashAvailable ? self.flashMode : .off
+
+            photoSettings.photoQualityPrioritization = .balanced
+            
+            
+            
+            if let photoOutputVideoConnection = photoOutput.connection(with: .video) {
+                photoOutputVideoConnection.videoRotationAngle = RotationAngle.portrait.rawValue
+            }
+            
+            photoOutput.capturePhoto(with: photoSettings, delegate: self)
+        }
+    }
+    
+
+    
+    private func updateSessionForCaptureDevice(_ captureDevice: AVCaptureDevice) {
+        guard isCaptureSessionConfigured else { return }
+        
+        captureSession.beginConfiguration()
+        defer { captureSession.commitConfiguration() }
+
+        for input in captureSession.inputs {
+            if let deviceInput = input as? AVCaptureDeviceInput {
+                captureSession.removeInput(deviceInput)
+            }
+        }
+        
+        if let deviceInput = deviceInputFor(device: captureDevice) {
+            if !captureSession.inputs.contains(deviceInput), captureSession.canAddInput(deviceInput) {
+                captureSession.addInput(deviceInput)
+            }
+        }
+        
+        updateVideoOutputConnection()
+    }
+    
+    private func deviceInputFor(device: AVCaptureDevice?) -> AVCaptureDeviceInput? {
+        guard let validDevice = device else { return nil }
+        do {
+            return try AVCaptureDeviceInput(device: validDevice)
+        } catch let error {
+            print("Error getting capture device input: \(error.localizedDescription)")
+            return nil
+        }
+    }
+    
+    private func configureCaptureSession(completionHandler: (_ success: Bool) -> Void) {
+        
+        var success = false
+        
+        self.captureSession.beginConfiguration()
+        
+        defer {
+            self.captureSession.commitConfiguration()
+            completionHandler(success)
+        }
+        
+        guard
+            let captureDevice = captureDevice,
+            let deviceInput = try? AVCaptureDeviceInput(device: captureDevice)
+        else {
+            print("Failed to obtain video input.")
+            return
+        }
+        
+        
+        let photoOutput = AVCapturePhotoOutput()
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        
+        
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "VideoDataOutputQueue"))
+        
+        guard captureSession.canAddInput(deviceInput) else {
+            print("Unable to add device input to capture session.")
+            return
+        }
+        guard captureSession.canAddOutput(photoOutput) else {
+            print("Unable to add photo output to capture session.")
+            return
+        }
+        guard captureSession.canAddOutput(videoOutput) else {
+            print("Unable to add video output to capture session.")
+            return
+        }
+        
+        captureSession.addInput(deviceInput)
+        captureSession.addOutput(photoOutput)
+        captureSession.addOutput(videoOutput)
+        
+        self.deviceInput = deviceInput
+        self.photoOutput = photoOutput
+        self.videoOutput = videoOutput
+
+        photoOutput.maxPhotoQualityPrioritization = .quality
+        
+        updateVideoOutputConnection()
+        
+        isCaptureSessionConfigured = true
+        
+        success = true
+    }
+    
+    
+    private func checkAuthorization() async -> Bool {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            print("Camera access authorized.")
+            return true
+        case .notDetermined:
+            print("Camera access not determined.")
+            sessionQueue.suspend()
+            let status = await AVCaptureDevice.requestAccess(for: .video)
+            sessionQueue.resume()
+            return status
+        case .denied:
+            print("Camera access denied.")
+            return false
+        case .restricted:
+            print("Camera library access restricted.")
+            return false
+        default:
+            return false
+        }
+    }
+    
+    private func updateVideoOutputConnection() {
+        if let videoOutput = videoOutput, let videoOutputConnection = videoOutput.connection(with: .video) {
+            if videoOutputConnection.isVideoMirroringSupported {
+                videoOutputConnection.isVideoMirrored = isUsingFrontCaptureDevice
+            }
+        }
+    }
+    
+    func toggleFlash() {
+        switch flashMode {
+        case .off:
+            flashMode = .on
+
+        case .on:
+            flashMode = .auto
+
+        case .auto:
+            flashMode = .off
+
+        @unknown default:
+            flashMode = .off
+        }
+    }
+    
+    
+    var flashModeIcon: String {
+        switch flashMode {
+        case .off:
+            return "bolt.slash"
+
+        case .on:
+            return "bolt.fill"
+
+        case .auto:
+            return "bolt.badge.a"
+
+        @unknown default:
+            return "bolt.slash"
+        }
+    }
+    
+    func setZoom(factor: CGFloat) {
+        guard let device = captureDevice else { return }
+
+        do {
+            try device.lockForConfiguration()
+
+            device.videoZoomFactor = max(
+                1.0,
+                min(factor, device.activeFormat.videoMaxZoomFactor)
+            )
+
+            device.unlockForConfiguration()
+
+        } catch {
+            print("Erro ao aplicar zoom")
+        }
+    }
+    
+    func setExposure(bias:Float) {
+    guard let device = captureDevice else {return }
+
+    do {
+    try device.lockForConfiguration()
+
+    let minBias = device.minExposureTargetBias
+    let maxBias = device.maxExposureTargetBias
+
+    let clampedBias = max(min(bias,maxBias),minBias)
+
+    device.setExposureTargetBias(clampedBias)
+
+    device.unlockForConfiguration()
+        }catch {
+    print("Erro ao ajustar exposição: \(error)")
+        }
+    }
+    
+
+}
+
+
+extension CameraManager: AVCapturePhotoCaptureDelegate {
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        
+        if let error = error {
+            print("Error capturing photo: \(error.localizedDescription)")
+            return
+        }
+        
+        addToPhotoStream?(photo)
+    }
+    
+}
+
+extension CameraManager: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = sampleBuffer.imageBuffer else { return }
+        connection.videoRotationAngle = RotationAngle.portrait.rawValue
+        addToPreviewStream?(CIImage(cvPixelBuffer: pixelBuffer))
     }
 }
 
-#Preview {
-    CameraManager()
+
+
+enum RotationAngle: CGFloat {
+    case portrait = 90
+    case portraitUpsideDown = 270
+    case landscapeRight = 180
+    case landscapeLeft = 0
 }
+
+
