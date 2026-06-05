7c41f051793ed7b6b32fa1d818f7ad995c2bd456
taiunara <anatainarabat@gmail.com>
2026-06-05 12:37:00 -0300

    feat: implement camera capture logic

---
+++ FlyingHigh/Model/CameraManager.swift
@@ -0,0 +1,18 @@
+//
+//  CameraManager.swift
+//  FlyingHigh
+//
+//  Created by Tainara Nascimento on 05/06/26.
+//
+
+import SwiftUI
+
+struct CameraManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraManager()
+}
