83395fb7a8735665e3ac530e3786f7466580a1f7
FelipepPrado <felipepdelimaa@gmail.com>
2026-06-23 11:15:33 -0300

    fix: alert in delet album

--- FlyingHigh/View/AlbumView.swift
+++ FlyingHigh/View/AlbumView.swift
@@ -103,7 +103,7 @@ struct AlbumView: View {
                 }
             }
         }message: {
-            Text("Após confirmar a ação, a experiência será deletado e não poderá ser recuperado.")
+            Text("Após confirmar a ação, a experiência será deletada e não poderá ser recuperada.")
         }
         
         .navigationDestination(isPresented: $showingCamera){
